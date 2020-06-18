663096459e4fdb7330b44172d998f54fd3518a32
mksajja <mk.sajja96@gmail.com>
2020-06-18 11:16:55 +0530

    ui changes in login, signup and home page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,17 +9,26 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   constructor() {
-    document.body.style.margin="0";
+   /* document.body.style.margin="0";
     document.body.style.padding="0";
     document.body.style.backgroundImage = "url('assets/images/loginbgimg.jpg')";
     document.body.style.backgroundPosition = "center";
     document.body.style.backgroundRepeat = "no-repeat";
     document.body.style.backgroundAttachment = "fixed";
     document.body.style.backgroundSize = "cover";
-
+*/
+    document.body.style.background="url('assets/images/restaurant.png')";
+    document.body.style.backgroundAttachment = "fixed";
+    document.body.style.margin="0";
+    document.body.style.padding="0";
+    document.body.style.backgroundSize = "cover";
+    /*document.body.style.minHeight="900px";*/
+    document.body.style.fontFamily="Open Sans', sans-serif";
   }
   ngOnDestroy(){
     document.body.style.backgroundImage = "none";
+    /*document.body.style.minHeight="";*/
+    document.body.style.fontFamily="";
   }
 
   ngOnInit(): void {
